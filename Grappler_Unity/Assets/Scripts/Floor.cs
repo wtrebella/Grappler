f57900a07e88abc156f531420f967216da089bf6
Whitaker Trebella <wtrebella@gmail.com>
2015-07-18 14:47:48 -0500

    got the floor looping with an object pool

---
+++ Grappler_Unity/Assets/Scripts/Floor.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Floor : MonoBehaviour {
+	[SerializeField] private float screenMargin = 10;
+	[SerializeField] private float tileSize = 2;
+	[SerializeField] private Transform floorTilePrefab;
+
+	private List<Transform> floorTiles;
+
+	private void Awake() {
+		floorTiles = new List<Transform>();
+	}
+
+	private void Start() {
+		CreateFloor();
+	}
+
+	private void Update() {
+		UpdateFloor();
+	}
+
+	private float GetMinX() {
+		float originX = GameScreen.instance.origin.x;
+		float minX = originX - screenMargin;
+		return minX;
+	}
+
+	private float GetMaxX() {
+		float originX = GameScreen.instance.origin.x;
+		float maxX = originX + GameScreen.instance.width + screenMargin;
+		return maxX;
+	}
+
+	private void AddFloorTileToEnd(Vector2 position) {
+		Transform floorTile = floorTilePrefab.Spawn();
+		floorTile.parent = transform;
+		floorTile.position = position;
+		floorTiles.Add(floorTile);
+	}
+
+	private void AddFloorTileToBeginning(Vector2 position) {
+		Transform floorTile = floorTilePrefab.Spawn();
+		floorTile.parent = transform;
+		floorTile.position = position;
+		floorTiles.Insert(0, floorTile);
+	}
+
+	private void CreateFloor() {
+		float nextX = GetMinX();
+
+		while (nextX < GetMaxX()) {
+			Vector2 position = new Vector2(nextX, 0);
+			AddFloorTileToEnd(position);
+			nextX += tileSize;
+		}
+	}
+
+	private void UpdateFloor() {
+		while (true) {
+			Transform floorTile = floorTiles[0];
+			if (floorTile.position.x >= GetMinX()) break;
+
+			floorTiles.RemoveAt(0);
+			floorTile.Recycle();
+			Transform lastFloorTile = floorTiles[floorTiles.Count - 1];
+			Vector2 position = new Vector2(lastFloorTile.transform.position.x + tileSize, 0);
+			AddFloorTileToEnd(position);
+		}
+
+		while (true) {
+			Transform floorTile = floorTiles[floorTiles.Count - 1];
+			if (floorTile.position.x <= GetMaxX()) break;
+			
+			floorTiles.RemoveAt(floorTiles.Count - 1);
+			floorTile.Recycle();
+			Transform firstFloorTile = floorTiles[0];
+			Vector2 position = new Vector2(firstFloorTile.transform.position.x - tileSize, 0);
+			AddFloorTileToBeginning(position);
+		}
+	}
+}
---
+++ Grappler_Unity/Assets/Scripts/GameScreen.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScreen : MonoBehaviour {
+	public static GameScreen instance;
+
+	[SerializeField] private tk2dCameraAnchor lowerLeftAnchor;
+	[SerializeField] private tk2dCameraAnchor upperRightAnchor;
+
+	private void Awake() {
+		instance = this;
+	}
+
+	public Vector2 center {
+		get {
+			Vector2 centerPoint = Vector2.zero;
+			centerPoint.x = origin.x + width / 2f;
+			centerPoint.y = origin.y + height / 2f;
+			return centerPoint;
+		}
+	}
+
+	public Vector2 origin {
+		get {
+			return lowerLeftAnchor.transform.position;
+		}
+	}
+	
+	public float width {
+		get {
+			return upperRightAnchor.transform.position.x - lowerLeftAnchor.transform.position.x;
+		}
+	}
+
+	public float height {
+		get {
+			return upperRightAnchor.transform.position.y - lowerLeftAnchor.transform.position.y;
+		}
+	}
+}
