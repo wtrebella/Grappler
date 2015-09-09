d7826d1e1c4b5fe5a7e3af3fba83bf7c6dcc570f
Whitaker Trebella <wtrebella@gmail.com>
2015-09-09 17:31:17 -0500

    front face creation now works with the 9-slice windows

--- Grappler_Unity/Assets/Scripts/BuildingMeshCreator.cs
+++ Grappler_Unity/Assets/Scripts/BuildingMeshCreator.cs
@@ -10,6 +10,7 @@ public class BuildingMeshCreator : MonoBehaviour {
 	[SerializeField] private Material mat3;
 
 	[SerializeField] private int maxBuildingPieces = 3;
+	[SerializeField] private Vector2 minMaxWindowMargin = new Vector2(2, 10);
 	[SerializeField] private Vector2 minWindowDimensions = new Vector2(2, 5);
 	[SerializeField] private Vector2 maxWindowDimensions = new Vector2(10, 25);
 	[SerializeField] private Vector3 minBuildingPieceSize = new Vector3(20, 50, 20);
@@ -23,6 +24,7 @@ public class BuildingMeshCreator : MonoBehaviour {
 	private MeshRenderer meshRenderer;
 	private UVRect windowUVRect;
 	private UVRect roofUVRect;
+	private UVRect windowMarginUVRect;
 
 	private void Awake() {
 		meshFilter = GetComponent<MeshFilter>();
@@ -57,15 +59,21 @@ public class BuildingMeshCreator : MonoBehaviour {
 	private void InitUVRects() {
 		windowUVRect = new UVRect();
 		windowUVRect.bottomLeft = new Vector2(0, 0);
-		windowUVRect.topLeft = new Vector2(0, 1);
-		windowUVRect.topRight = new Vector2(0.5f, 1);
-		windowUVRect.bottomRight = new Vector2(0.5f, 0);
+		windowUVRect.topLeft = new Vector2(0, 0.5f);
+		windowUVRect.topRight = new Vector2(0.25f, 0.5f);
+		windowUVRect.bottomRight = new Vector2(0.25f, 0);
 
 		roofUVRect = new UVRect();
 		roofUVRect.bottomLeft = new Vector2(0.5f, 0);
 		roofUVRect.topLeft = new Vector2(0.5f, 1);
 		roofUVRect.topRight = new Vector2(1, 1);
 		roofUVRect.bottomRight = new Vector2(1, 0);
+
+		windowMarginUVRect = new UVRect();
+		windowMarginUVRect.bottomLeft = new Vector2(0.25f, 0);
+		windowMarginUVRect.topLeft = new Vector2(0.25f, 0.5f);
+		windowMarginUVRect.topRight = new Vector2(0.5f, 0.5f);
+		windowMarginUVRect.bottomRight = new Vector2(0.5f, 0);
 	}
 
 	private void CreateMesh() {
@@ -74,12 +82,13 @@ public class BuildingMeshCreator : MonoBehaviour {
 		Vector3 previousDimensions = Vector3.zero;
 		Vector3 previousOrigin = Vector3.zero;
 
-		Vector2 windowDimensions = GetRandomWindowDimensions();
+		Vector2 marginSize = GetRandomMarginSize();
+		Vector2 windowSize = GetRandomWindowSize();
 
 		for (int i = 0; i < buildingPieces; i++) {
-			IntVector3 numWindows = GetRandomNumWindows(windowDimensions);
-			Vector3 buildingPieceDimensions = GetBuildingPieceDimensions(numWindows, windowDimensions);
-			float inset = GetRandomInset(windowDimensions);
+			Vector3 buildingPieceDimensions = GetBuildingPieceDimensions();
+			IntVector3 numWindows = GetNumWindows(buildingPieceDimensions, windowSize, marginSize);
+			float inset = GetRandomInset(windowSize, marginSize);
 
 			Vector3 origin = new Vector3(previousOrigin.x + previousDimensions.x, 0, inset);
 			Vector3 frontFaceOrigin = origin;
@@ -87,10 +96,10 @@ public class BuildingMeshCreator : MonoBehaviour {
 			Vector3 leftFaceOrigin = new Vector3(origin.x, origin.y, origin.z + buildingPieceDimensions.z);
 			Vector3 topFaceOrigin = new Vector3(origin.x, origin.y + buildingPieceDimensions.y, origin.z);
 
-			CreateFrontFace(frontFaceOrigin, numWindows, windowDimensions);
-			CreateRightFace(rightFaceOrigin, numWindows, windowDimensions);
-			CreateLeftFace(leftFaceOrigin, numWindows, windowDimensions);
-			CreateTopFace(topFaceOrigin, numWindows, windowDimensions);
+			CreateFrontFace(frontFaceOrigin, numWindows, windowSize, marginSize);
+//			CreateRightFace(rightFaceOrigin, numWindows, windowSize, windowMargins);
+//			CreateLeftFace(leftFaceOrigin, numWindows, windowSize, windowMargins);
+//			CreateTopFace(topFaceOrigin, numWindows, windowSize, windowMargins);
 
 			previousDimensions = buildingPieceDimensions;
 			previousOrigin = origin;
@@ -103,36 +112,253 @@ public class BuildingMeshCreator : MonoBehaviour {
 		meshFilter.mesh.RecalculateBounds();
 		meshFilter.mesh.RecalculateNormals();
 	}
+	
+	private void CreateFrontFace(Vector3 origin, IntVector3 numWindows, Vector2 windowSize, Vector2 marginSize) {
+		IntVector3 chunkCount = new IntVector3(
+			Mathf.Max(numWindows.x, 1),
+			Mathf.Max(numWindows.y, 1),
+			Mathf.Max(numWindows.z, 1)
+		);
 
-	private void CreateFrontFace(Vector3 origin, IntVector3 numWindows, Vector2 windowDimensions) {
-		for (int yWindow = 0; yWindow < numWindows.y; yWindow++) {
-			for (int xWindow = 0; xWindow < numWindows.x; xWindow++) {
+		for (int y = 0; y <= chunkCount.y; y++) {
+			for (int x = 0; x <= chunkCount.x; x++) {
+				if (chunkCount.x == 1 || chunkCount.y == 1) {
+
+				}
+				else {
+					int startIndex = verts.Count;
+					bool atLastX = x == chunkCount.x;
+					bool atLastY = y == chunkCount.y;
+
+					Vector2 marginCornerOrigin = new Vector2(
+						origin.x + x * (windowSize.x + marginSize.x),
+						origin.y + y * (windowSize.y + marginSize.y)
+					);
+
+					Vector2 marginHorizontalOrigin = new Vector2(
+						origin.x + x * (windowSize.x + marginSize.x) + marginSize.x,
+						origin.y + y * (windowSize.y + marginSize.y)
+					);
+
+					Vector2 marginVerticalOrigin = new Vector2(
+						origin.x + x * (windowSize.x + marginSize.x),
+						origin.y + y * (windowSize.y + marginSize.y) + marginSize.y
+					);
+	
+					Vector2 windowOrigin = new Vector2(
+						origin.x + x * (windowSize.x + marginSize.x) + marginSize.x,
+						origin.y + y * (windowSize.y + marginSize.y) + marginSize.y
+					);
+
+					// corner margin
+					verts.Add(new Vector3(
+						marginCornerOrigin.x,
+						marginCornerOrigin.y,
+						origin.z
+					));
+					
+					verts.Add(new Vector3(
+						marginCornerOrigin.x,
+						marginCornerOrigin.y + marginSize.y,
+						origin.z
+					));
+					
+					verts.Add(new Vector3(
+						marginCornerOrigin.x + marginSize.x,
+						marginCornerOrigin.y + marginSize.y,
+						origin.z
+					));
+					
+					verts.Add(new Vector3(
+						marginCornerOrigin.x + marginSize.x,
+						marginCornerOrigin.y,
+						origin.z
+					));
+
+					uvs.Add(windowMarginUVRect.bottomLeft);
+					uvs.Add(windowMarginUVRect.topLeft);
+					uvs.Add(windowMarginUVRect.topRight);
+					uvs.Add(windowMarginUVRect.bottomRight);
+					
+					tris.Add(startIndex + 0);
+					tris.Add(startIndex + 1);
+					tris.Add(startIndex + 2);
+					
+					tris.Add(startIndex + 2);
+					tris.Add(startIndex + 3);
+					tris.Add(startIndex + 0);
+
+					startIndex += 4;
+
+					// horizontal margin
+					if (!atLastX) {
+						verts.Add(new Vector3(
+							marginHorizontalOrigin.x,
+							marginHorizontalOrigin.y,
+							origin.z
+						));
+						
+						verts.Add(new Vector3(
+							marginHorizontalOrigin.x,
+							marginHorizontalOrigin.y + marginSize.y,
+							origin.z
+						));
+						
+						verts.Add(new Vector3(
+							marginHorizontalOrigin.x + windowSize.x,
+							marginHorizontalOrigin.y + marginSize.y,
+							origin.z
+						));
+						
+						verts.Add(new Vector3(
+							marginHorizontalOrigin.x + windowSize.x,
+							marginHorizontalOrigin.y,
+							origin.z
+						));
+
+						uvs.Add(windowMarginUVRect.bottomLeft);
+						uvs.Add(windowMarginUVRect.topLeft);
+						uvs.Add(windowMarginUVRect.topRight);
+						uvs.Add(windowMarginUVRect.bottomRight);
+						
+						tris.Add(startIndex + 0);
+						tris.Add(startIndex + 1);
+						tris.Add(startIndex + 2);
+						
+						tris.Add(startIndex + 2);
+						tris.Add(startIndex + 3);
+						tris.Add(startIndex + 0);
+						
+						startIndex += 4;
+					}
+
+					// vertical margin
+					if (!atLastY) {
+						verts.Add(new Vector3(
+							marginVerticalOrigin.x,
+							marginVerticalOrigin.y,
+							origin.z
+						));
+						
+						verts.Add(new Vector3(
+							marginVerticalOrigin.x,
+							marginVerticalOrigin.y + windowSize.y,
+							origin.z
+						));
+					
+						verts.Add(new Vector3(
+							marginVerticalOrigin.x + marginSize.x,
+							marginVerticalOrigin.y + windowSize.y,
+							origin.z
+						));
+						
+						verts.Add(new Vector3(
+							marginVerticalOrigin.x + marginSize.x,
+							marginVerticalOrigin.y,
+							origin.z
+						));
+
+						uvs.Add(windowMarginUVRect.bottomLeft);
+						uvs.Add(windowMarginUVRect.topLeft);
+						uvs.Add(windowMarginUVRect.topRight);
+						uvs.Add(windowMarginUVRect.bottomRight);
+						
+						tris.Add(startIndex + 0);
+						tris.Add(startIndex + 1);
+						tris.Add(startIndex + 2);
+						
+						tris.Add(startIndex + 2);
+						tris.Add(startIndex + 3);
+						tris.Add(startIndex + 0);
+						
+						startIndex += 4;
+					}
+
+					// window
+					if (!atLastX && !atLastY) {
+						verts.Add(new Vector3(
+							windowOrigin.x,
+							windowOrigin.y,
+							origin.z
+						));
+						
+						verts.Add(new Vector3(
+							windowOrigin.x,
+							windowOrigin.y + windowSize.y,
+							origin.z
+						));
+						
+						verts.Add(new Vector3(
+							windowOrigin.x + windowSize.x,
+							windowOrigin.y + windowSize.y,
+							origin.z
+						));
+						
+						verts.Add(new Vector3(
+							windowOrigin.x + windowSize.x,
+							windowOrigin.y,
+							origin.z
+						));
+
+						uvs.Add(windowUVRect.bottomLeft);
+						uvs.Add(windowUVRect.topLeft);
+						uvs.Add(windowUVRect.topRight);
+						uvs.Add(windowUVRect.bottomRight);
+						
+						tris.Add(startIndex + 0);
+						tris.Add(startIndex + 1);
+						tris.Add(startIndex + 2);
+						
+						tris.Add(startIndex + 2);
+						tris.Add(startIndex + 3);
+						tris.Add(startIndex + 0);
+					}
+				}
+			}
+		}
+	}
+
+	private void CreateRightFace(Vector3 origin, IntVector3 numWindows, Vector2 windowSize, Vector2 windowMargins) {
+		Vector3 chunkDimensions = new Vector3(
+			numWindows.x > 0 ? (windowSize.x + windowMargins.x) : minBuildingPieceSize.x,
+			numWindows.y > 0 ? (windowSize.y + windowMargins.y) : minBuildingPieceSize.y,
+			numWindows.z > 0 ? (windowSize.x + windowMargins.x) : minBuildingPieceSize.z
+		);
+		
+		IntVector3 chunkCount = new IntVector3(
+			Mathf.Max(numWindows.x, 1),
+			Mathf.Max(numWindows.y, 1),
+			Mathf.Max(numWindows.z, 1)
+		);
+
+		for (int y = 0; y < chunkCount.y; y++) {
+			for (int z = 0; z < chunkCount.z; z++) {
 				int startIndex = verts.Count;
 				
 				verts.Add(new Vector3(
-					origin.x + (xWindow + 0) * windowDimensions.x,
-					origin.y + (yWindow + 0) * windowDimensions.y,
-					origin.z
-					));
+					origin.x,
+					origin.y + (y + 0) * chunkDimensions.y,
+					origin.z + (z + 0) * chunkDimensions.z
+				));
 				
 				verts.Add(new Vector3(
-					origin.x + (xWindow + 0) * windowDimensions.x,
-					origin.y + (yWindow + 1) * windowDimensions.y,
-					origin.z
-					));
+					origin.x,
+					origin.y + (y + 1) * chunkDimensions.y,
+					origin.z + (z + 0) * chunkDimensions.z
+				));
 				
 				verts.Add(new Vector3(
-					origin.x + (xWindow + 1) * windowDimensions.x,
-					origin.y + (yWindow + 1) * windowDimensions.y,
-					origin.z
-					));
+					origin.x,
+					origin.y + (y + 1) * chunkDimensions.y,
+					origin.z + (z + 1) * chunkDimensions.z
+				));
 				
 				verts.Add(new Vector3(
-					origin.x + (xWindow + 1) * windowDimensions.x,
-					origin.y + (yWindow + 0) * windowDimensions.y,
-					origin.z
-					));
-				
+					origin.x,
+					origin.y + (y + 0) * chunkDimensions.y,
+					origin.z + (z + 1) * chunkDimensions.z
+				));
+
 				uvs.Add(windowUVRect.bottomLeft);
 				uvs.Add(windowUVRect.topLeft);
 				uvs.Add(windowUVRect.topRight);
@@ -149,34 +375,46 @@ public class BuildingMeshCreator : MonoBehaviour {
 		}
 	}
 
-	private void CreateRightFace(Vector3 origin, IntVector3 numWindows, Vector2 windowDimensions) {
-		for (int yWindow = 0; yWindow < numWindows.y; yWindow++) {
-			for (int zWindow = 0; zWindow < numWindows.z; zWindow++) {
+	private void CreateLeftFace(Vector3 origin, IntVector3 numWindows, Vector2 windowSize, Vector2 windowMargins) {
+		Vector3 chunkDimensions = new Vector3(
+			numWindows.x > 0 ? (windowSize.x + windowMargins.x) : minBuildingPieceSize.x,
+			numWindows.y > 0 ? (windowSize.y + windowMargins.y) : minBuildingPieceSize.y,
+			numWindows.z > 0 ? (windowSize.x + windowMargins.x) : minBuildingPieceSize.z
+		);
+		
+		IntVector3 chunkCount = new IntVector3(
+			Mathf.Max(numWindows.x, 1),
+			Mathf.Max(numWindows.y, 1),
+			Mathf.Max(numWindows.z, 1)
+		);
+
+		for (int y = 0; y < chunkCount.y; y++) {
+			for (int z = 0; z < chunkCount.z; z++) {
 				int startIndex = verts.Count;
 				
 				verts.Add(new Vector3(
 					origin.x,
-					origin.y + (yWindow + 0) * windowDimensions.y,
-					origin.z + (zWindow + 0) * windowDimensions.x
-					));
+					origin.y + (y + 0) * chunkDimensions.y,
+					origin.z - (z + 0) * chunkDimensions.z
+				));
 				
 				verts.Add(new Vector3(
 					origin.x,
-					origin.y + (yWindow + 1) * windowDimensions.y,
-					origin.z + (zWindow + 0) * windowDimensions.x
-					));
+					origin.y + (y + 1) * chunkDimensions.y,
+					origin.z - (z + 0) * chunkDimensions.z
+				));
 				
 				verts.Add(new Vector3(
 					origin.x,
-					origin.y + (yWindow + 1) * windowDimensions.y,
-					origin.z + (zWindow + 1) * windowDimensions.x
-					));
+					origin.y + (y + 1) * chunkDimensions.y,
+					origin.z - (z + 1) * chunkDimensions.z
+				));
 				
 				verts.Add(new Vector3(
 					origin.x,
-					origin.y + (yWindow + 0) * windowDimensions.y,
-					origin.z + (zWindow + 1) * windowDimensions.x
-					));
+					origin.y + (y + 0) * chunkDimensions.y,
+					origin.z - (z + 1) * chunkDimensions.z
+				));
 
 				uvs.Add(windowUVRect.bottomLeft);
 				uvs.Add(windowUVRect.topLeft);
@@ -194,77 +432,44 @@ public class BuildingMeshCreator : MonoBehaviour {
 		}
 	}
 
-	private void CreateLeftFace(Vector3 origin, IntVector3 numWindows, Vector2 windowDimensions) {
-		for (int yWindow = 0; yWindow < numWindows.y; yWindow++) {
-			for (int zWindow = 0; zWindow < numWindows.z; zWindow++) {
-				int startIndex = verts.Count;
-				
-				verts.Add(new Vector3(
-					origin.x,
-					origin.y + (yWindow + 0) * windowDimensions.y,
-					origin.z - (zWindow + 0) * windowDimensions.x
-					));
-				
-				verts.Add(new Vector3(
-					origin.x,
-					origin.y + (yWindow + 1) * windowDimensions.y,
-					origin.z - (zWindow + 0) * windowDimensions.x
-					));
-				
-				verts.Add(new Vector3(
-					origin.x,
-					origin.y + (yWindow + 1) * windowDimensions.y,
-					origin.z - (zWindow + 1) * windowDimensions.x
-					));
-				
-				verts.Add(new Vector3(
-					origin.x,
-					origin.y + (yWindow + 0) * windowDimensions.y,
-					origin.z - (zWindow + 1) * windowDimensions.x
-					));
+	private void CreateTopFace(Vector3 origin, IntVector3 numWindows, Vector2 windowSize, Vector2 windowMargins) {
+		Vector3 chunkDimensions = new Vector3(
+			numWindows.x > 0 ? (windowSize.x + windowMargins.x) : minBuildingPieceSize.x,
+			numWindows.y > 0 ? (windowSize.y + windowMargins.y) : minBuildingPieceSize.y,
+			numWindows.z > 0 ? (windowSize.x + windowMargins.x) : minBuildingPieceSize.z
+		);
 
-				uvs.Add(windowUVRect.bottomLeft);
-				uvs.Add(windowUVRect.topLeft);
-				uvs.Add(windowUVRect.topRight);
-				uvs.Add(windowUVRect.bottomRight);
-				
-				tris.Add(startIndex + 0);
-				tris.Add(startIndex + 1);
-				tris.Add(startIndex + 2);
-				
-				tris.Add(startIndex + 2);
-				tris.Add(startIndex + 3);
-				tris.Add(startIndex + 0);
-			}
-		}
-	}
+		IntVector3 chunkCount = new IntVector3(
+			Mathf.Max(numWindows.x, 1),
+			Mathf.Max(numWindows.y, 1),
+			Mathf.Max(numWindows.z, 1)
+		);
 
-	private void CreateTopFace(Vector3 origin, IntVector3 numWindows, Vector2 windowDimensions) {
 		int startIndex = verts.Count;
 		
 		verts.Add(new Vector3(
 			origin.x,
 			origin.y,
 			origin.z
-			));
+		));
 		
 		verts.Add(new Vector3(
 			origin.x,
 			origin.y,
-			origin.z + numWindows.z * windowDimensions.x
-			));
+			origin.z + chunkCount.z * chunkDimensions.z
+		));
 		
 		verts.Add(new Vector3(
-			origin.x + numWindows.x * windowDimensions.x,
+			origin.x + chunkCount.x * chunkDimensions.x,
 			origin.y,
-			origin.z + numWindows.z * windowDimensions.x
-			));
+			origin.z + chunkCount.z * chunkDimensions.z
+		));
 		
 		verts.Add(new Vector3(
-			origin.x + numWindows.x * windowDimensions.x,
+			origin.x + chunkCount.x * chunkDimensions.x,
 			origin.y,
 			origin.z
-			));
+		));
 		
 		uvs.Add(roofUVRect.bottomLeft);
 		uvs.Add(roofUVRect.topLeft);
@@ -280,15 +485,15 @@ public class BuildingMeshCreator : MonoBehaviour {
 		tris.Add(startIndex + 0);
 	}
 
-	private Vector3 GetBuildingPieceDimensions(IntVector3 numWindows, Vector2 windowDimensions) {
+	private Vector3 GetBuildingPieceDimensions() {
 		Vector3 v = new Vector3();
-		v.x = numWindows.x * windowDimensions.x;
-		v.y = numWindows.y * windowDimensions.y;
-		v.z = numWindows.z * windowDimensions.x;
+		v.x = Random.Range(minBuildingPieceSize.x, maxBuildingPieceSize.x);
+		v.y = Random.Range(minBuildingPieceSize.y, maxBuildingPieceSize.y);
+		v.z = Random.Range(minBuildingPieceSize.z, maxBuildingPieceSize.z);
 		return v;
 	}
 
-	private Vector2 GetRandomWindowDimensions() {
+	private Vector2 GetRandomWindowSize() {
 		Vector2 windowDimensions = new Vector2(
 			Random.Range(minWindowDimensions.x, maxWindowDimensions.x),
 			Random.Range(minWindowDimensions.y, maxWindowDimensions.y)
@@ -297,30 +502,23 @@ public class BuildingMeshCreator : MonoBehaviour {
 		return windowDimensions;
 	}
 
-	private IntVector3 GetRandomNumWindows(Vector2 windowDimensions) {
-		IntVector3 minNumWindows = new IntVector3(
-			minBuildingPieceSize.x / windowDimensions.x,
-			minBuildingPieceSize.y / windowDimensions.y,
-			minBuildingPieceSize.z / windowDimensions.x
-		);
-
-		IntVector3 maxNumWindows = new IntVector3(
-			maxBuildingPieceSize.x / windowDimensions.x,
-			maxBuildingPieceSize.y / windowDimensions.y,
-			maxBuildingPieceSize.z / windowDimensions.x
-		);
-		
-		IntVector3 v = new IntVector3();
-		v.x = Random.Range(minNumWindows.x, maxNumWindows.x);
-		v.y = Random.Range(minNumWindows.y, maxNumWindows.y);
-		v.z = Random.Range(minNumWindows.z, maxNumWindows.z);
-
-		return v;
+	private Vector2 GetRandomMarginSize() {
+		return new Vector2(Random.Range(minMaxWindowMargin.x, minMaxWindowMargin.y), Random.Range(minMaxWindowMargin.x, minMaxWindowMargin.y));
 	}
 
-	private float GetRandomInset(Vector2 windowDimensions) {
+	private IntVector3 GetNumWindows(Vector3 buildingPieceDimensions, Vector2 windowSize, Vector2 windowMargins) {
+		IntVector3 numWindows = new IntVector3(
+			(buildingPieceDimensions.x - windowMargins.x) / (windowSize.x + windowMargins.x),
+			(buildingPieceDimensions.y - windowMargins.y) / (windowSize.y + windowMargins.y),
+			(buildingPieceDimensions.z - windowMargins.x) / (windowSize.x + windowMargins.x)
+		);
+
+		return numWindows;
+	}
+
+	private float GetRandomInset(Vector2 windowSize, Vector2 windowMargins) {
 		int numWindowInset = Random.Range(0, maxNumWindowInset + 1);
-		float inset = numWindowInset * windowDimensions.x;
+		float inset = numWindowInset * (windowSize.x + windowMargins.x) + windowMargins.x;
 		return inset;	
 	}
 
