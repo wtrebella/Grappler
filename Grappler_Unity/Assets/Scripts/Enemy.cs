047f547aba1e8c85d970e2d2b4004b98f5720d44
Whitaker Trebella <wtrebella@gmail.com>
2015-12-21 16:35:32 -0600

    smoothed out the enemy

--- Grappler_Unity/Assets/Scripts/Enemy.cs
+++ Grappler_Unity/Assets/Scripts/Enemy.cs
@@ -4,16 +4,18 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
 	private MountainChunk mountainChunk;
 	private float linePosition;
+	private Vector3 smoothVelocity;
 
 	public void SetMountainChunk(MountainChunk mountainChunk) {
 		this.mountainChunk = mountainChunk;
-		SetLinePosition(0f);
+		SetLinePosition(0f, false);
 	}
 
-	private void SetLinePosition(float linePosition) {
+	private void SetLinePosition(float linePosition, bool smoothDamp = true) {
 		this.linePosition = linePosition;
 		Vector3 positionAlongLine = mountainChunk.GetPositionAlongLine(linePosition);
 		positionAlongLine.z = -0.01f;
+		if (smoothDamp) positionAlongLine = Vector3.SmoothDamp(transform.position, positionAlongLine, ref smoothVelocity, 0.13f);
 		transform.position = positionAlongLine;
 	}
 
