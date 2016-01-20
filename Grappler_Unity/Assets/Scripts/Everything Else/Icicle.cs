d83204d69c0ea75a75420bf8f21e191adcc8f4e9
Whitaker Trebella <wtrebella@gmail.com>
2016-01-20 01:16:42 -0600

    [hopefully] fixed the icicles-getting-stuck-inside-body problem

--- Grappler_Unity/Assets/Scripts/Everything Else/Icicle.cs
+++ Grappler_Unity/Assets/Scripts/Everything Else/Icicle.cs
@@ -4,6 +4,6 @@ using System.Collections;
 public class Icicle : MonoBehaviour {
 	private void OnCollisionEnter2D(Collision2D collision) {
 		Rigidbody2D rigid = GetComponent<Rigidbody2D>();
-		rigid.isKinematic = false;
+		rigid.gravityScale = 1;
 	}
 }
