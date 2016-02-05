6c5dedc5b51eadb4d39a5c637b6b2fa0b8db6eb7
Whitaker Trebella <wtrebella@gmail.com>
2016-02-05 12:17:52 -0600

    variety in trees; trees now clump together

--- Grappler_Unity/Assets/_Scripts/Generators/TreeGenerator.cs
+++ Grappler_Unity/Assets/_Scripts/Generators/TreeGenerator.cs
@@ -3,18 +3,25 @@ using System.Collections;
 
 public class TreeGenerator : Generator {
 	[SerializeField] private GroundChunkGenerator groundChunkGenerator;
-
+	[SerializeField] private int maxTreesPerChunk = 10;
+	[SerializeField] private float chunkProbability = 0.1f;
+	[SerializeField] private float treeProbability = 0.8f;
 	private void Awake() {
 		base.BaseAwake();
 		groundChunkGenerator.SignalGroundChunkGenerated += HandleGroundChunkCreated;
 	}
 
 	private void HandleGroundChunkCreated(GroundChunk chunk) {
+		if (Random.value > chunkProbability) return;
+
 		var linePoints = chunk.GetListOfLinePoints();
-		float chance = 0.3f;
-		for (int i = 5; i < linePoints.Count; i++) {
+		int linePointsCount = linePoints.Count;
+		int startIndex = Random.Range(0, linePointsCount - 1);
+		int endIndex = Mathf.Min(linePointsCount - 1, startIndex + maxTreesPerChunk);
+		for (int i = startIndex; i < endIndex; i++) {
+			if (Random.value > treeProbability) continue;
 			Point point = linePoints[i];
-			if (Random.value < chance) GenerateTree(point);
+			GenerateTree(point);
 		}
 	}
 
@@ -23,6 +30,9 @@ public class TreeGenerator : Generator {
 		Vector3 position = chunkLinePoint.pointVector.ToVector3();
 		Rigidbody2D rigid = item.GetComponentInChildren<Rigidbody2D>();
 		rigid.isKinematic = true;
+		item.transform.localScale = new Vector2(Random.Range(0.6f, 1.0f), Random.Range(0.6f, 1.0f));
+		float color = Random.Range(0.8f, 1.0f);
+		item.GetComponentInChildren<tk2dSprite>().color = new Color(Random.Range(0.95f, 1.0f), Random.Range(0.95f, 1.0f), Random.Range(0.95f, 1.0f));
 		item.transform.position = position;
 		rigid.isKinematic = false;
 	}
