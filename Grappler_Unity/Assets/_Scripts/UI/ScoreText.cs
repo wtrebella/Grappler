be87c410895d44c79bdace9a305db96097ea5e51
Whitaker Trebella <wtrebella@gmail.com>
2016-02-05 18:12:17 -0600

    added some basic score text and a reset button

---
+++ Grappler_Unity/Assets/_Scripts/UI/ScoreText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour {
+	[SerializeField] private Transform objectToMeasure;
+
+	private Text label;
+	private float dist;
+	private float startOffset;
+
+	private void Awake() {
+		label = GetComponent<Text>();
+		startOffset = -objectToMeasure.position.x;
+	}
+
+	private void Update() {
+		dist = objectToMeasure.position.x + startOffset;
+		label.text = ((int)(dist / 10)).ToString();
+	}
+}
