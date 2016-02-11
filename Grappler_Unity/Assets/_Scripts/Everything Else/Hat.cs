51b90f5f68ce67b0c36c6c0cc462140c28ba03d0
Whitaker Trebella <wtrebella@gmail.com>
2016-02-11 17:50:38 -0600

    starting to get some hat switching in

---
+++ Grappler_Unity/Assets/_Scripts/Everything Else/Hat.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Spine;
+
+public class Hat : MonoBehaviour {
+	[SerializeField] private SkeletonAnimation stickmanTop;
+
+	private void Awake() {
+		
+	}
+
+	private void Start() {
+		stickmanTop.skeleton.SetAttachment("hat", "hats/sombrero");
+
+	}
+	
+	private void Update() {
+
+	}
+}
