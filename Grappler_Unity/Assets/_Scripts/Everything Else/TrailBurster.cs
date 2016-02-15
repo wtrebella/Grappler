5c73c994130cac55d2fb4f8c3d823edcd58ca7a4
Whitaker Trebella <wtrebella@gmail.com>
2016-02-15 03:31:46 -0600

    made some simplifications and cohesion to ui; added some random various icons for ui use; gave the trail a pulse

--- Grappler_Unity/Assets/_Scripts/Everything Else/TrailBurster.cs
+++ Grappler_Unity/Assets/_Scripts/Everything Else/TrailBurster.cs
@@ -5,9 +5,37 @@ public class TrailBurster : MonoBehaviour {
 	[SerializeField] private TrailRenderer trail;
 
 	public void OnPushBack() {
-		trail.material.color = Color.red;
-		trail.startWidth = 2;
+		Pulse();
 	}
+
+	public void Pulse() {
+		StopPulsing();
+		StartCoroutine("PulseCoroutine");
+	}
+
+	private void StopPulsing() {
+		StopCoroutine("PulseCoroutine");
+		Go.killAllTweensWithTarget(trail);
+		Go.killAllTweensWithTarget(trail.material);
+	}
+
+	private IEnumerator PulseCoroutine() {
+		float duration1 = 0.2f;
+		float duration2 = 0.5f;
+
+		Go.to(trail.material, duration1, new GoTweenConfig().colorProp("color", new Color(1.0f, 1.0f, 0.1f, 1.0f)).setEaseType(GoEaseType.SineInOut));
+		Go.to(trail, duration1, new GoTweenConfig().floatProp("startWidth", 2.0f).setEaseType(GoEaseType.SineInOut));
+
+		yield return new WaitForSeconds(duration1);
+		yield return new WaitForEndOfFrame();
+
+		Go.to(trail.material, duration2, new GoTweenConfig().setDelay(duration1).colorProp("color", new Color(1.0f, 1.0f, 1.0f, 0.0f)).setEaseType(GoEaseType.SineInOut));
+		Go.to(trail, duration2, new GoTweenConfig().setDelay(duration1).floatProp("startWidth", 0.5f).setEaseType(GoEaseType.SineInOut));
+	}
+
+	// TODO: make it so the pulse's size and color change relative to the strength of the push back!
+	// this way it will be self-evident what it means, because the user will inevitably do swings at
+	// different speeds, and therefore notice the change in trail.
 
 	private void Awake() {
 
