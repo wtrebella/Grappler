6dc4cb490246f6b1e7aa44f0e945d809248b7fc1
Whitaker Trebella <wtrebella@gmail.com>
2016-02-29 18:57:50 -0600

    now there's a uimanager

---
+++ Grappler_Unity/Assets/_Scripts/UI/UIManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+public class UIManager : Singleton<UIManager> {
+	[SerializeField] private Canvas UICanvas;
+
+	private Dictionary<System.Type, List<PanelBase>> panelDictionary = new Dictionary<System.Type, List<PanelBase>>();
+
+	private void Awake() {
+		DontDestroyElseKill(this);
+
+		foreach(PanelBase panel in GetComponentsInChildren<PanelBase>(true)) {
+			if(panelDictionary.ContainsKey(panel.GetType())) panelDictionary[panel.GetType()].Add(panel);
+			else {
+				List<PanelBase> newPanelList = new List<PanelBase>();
+				newPanelList.Add(panel);
+
+				panelDictionary.Add(panel.GetType(), newPanelList);
+			}
+
+			panel.gameObject.SetActive(false);
+		}
+	}
+
+	public static T GetPanelOfType<T>() where T : PanelBase {
+		return (T)instance.panelDictionary[typeof(T)].FirstOrDefault();
+	}
+
+	public static List<T> GetPanelsOfType<T>() where T : PanelBase {
+		return instance.panelDictionary[typeof(T)].Cast<T>().ToList();
+	}
+}
