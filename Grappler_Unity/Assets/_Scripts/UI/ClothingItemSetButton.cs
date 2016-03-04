d29ae937966b6b3b6f07526b160f30f8b4b6be87
Whitaker Trebella <wtrebella@gmail.com>
2016-03-04 00:00:46 -0600

    you can now click on the objects to wear them

--- Grappler_Unity/Assets/_Scripts/UI/ClothingItemSetButton.cs
+++ Grappler_Unity/Assets/_Scripts/UI/ClothingItemSetButton.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ClothingItemSetButton : MonoBehaviour {
+	[SerializeField] private Button button;
 	[SerializeField] private ClothingItemSetUISprite sprite;
 
+	private ClothingManager clothingManager;
 	private ClothingItemSet itemSet;
+
+	private void Awake() {
+		clothingManager = GameObject.FindObjectOfType<ClothingManager>();
+	}
 
 	public void SetClothingItemSet(ClothingItemSet itemSet) {
 		this.itemSet = itemSet;
 		sprite.SetClothingItemSet(itemSet);
+		if (clothingManager != null) button.onClick.AddListener(() => clothingManager.EquipItemSet(itemSet));
 	}
 }
