f94f7e408b766e6eecddeab7073cf2a73e49fd71
Whitaker Trebella <wtrebella@gmail.com>
2016-04-26 22:01:24 -0500

    made a cool fluid polished ui alert; made some changes to the score text

---
+++ Grappler_Unity/Assets/_Scripts/Project/UI/Managers/UIManagerGlobal.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManagerGlobal : UIManager {
+
+	public void ShowMenu() {
+		Menu menu = GetPanelOfType<Menu>();
+		menu.SetTitle("Fucker!");
+		menu.SetDescription("This is the definition of a fucker. Never underestimate them. They will fuck you. Over and over.");
+		menu.SetHeaderColor(new Color(0.3f, 0.7f, 1.0f));
+		menu.Show();
+	}
+
+	public void HideMenu() {
+		Menu menu = GetPanelOfType<Menu>();
+		menu.Hide();
+	}
+
+	private void Update() {
+		if (Input.GetKeyDown(KeyCode.Space)) ShowMenu();
+	}
+}
