bde74608fa499375ad7d0775fc48963694bbdf2a
Whitaker Trebella <wtrebella@gmail.com>
2016-04-28 13:39:33 -0500

    simplified and improved rock slide physics

--- Grappler_Unity/Assets/_Scripts/Project/Environment/Rock Slide/RockSlide.cs
+++ Grappler_Unity/Assets/_Scripts/Project/Environment/Rock Slide/RockSlide.cs
@@ -5,24 +5,24 @@ using UnityEngine.Events;
 // TODO: get rid of hasStarted and extract it out into game manager
 
 public class RockSlide : MonoBehaviour {
-	public UnityEventWithFloat OnPushBack;
-
 	[SerializeField] private Transform startPoint;
-	[SerializeField] private Follow follow;
 	[SerializeField] private WhitUpdateType updateType = WhitUpdateType.Update;
-	[SerializeField] private FloatRange offsetChangeRateRange = new FloatRange(-4, 4);
 	[SerializeField] private Transform playerBody;
-	[SerializeField] private float slowZoneDistanceFromPlayer = 10;
-	[SerializeField] private float slowZoneMultiplier = 0.1f;
-	[SerializeField] private float pushBackAmount = -10;
+	[SerializeField] private float slowZoneWidth = 40;
+	[SerializeField] private float slowZoneSpeed = 80;
+	[SerializeField] private float fastZoneSpeed = 120;
 
 	private bool hasStarted = false;
-
+	private bool isMoving = false;
 	private float invertedSpeedPercent = 1;
 	private float currentOffsetChangeRate = 0;
 
+	public void StartMoving() {
+		isMoving = true;
+	}
+
 	public void StopMoving() {
-		follow.enabled = false;
+		isMoving = false;
 	}
 
 	public void OnSpeedPercentChanged(float percent) {
@@ -34,65 +34,44 @@ public class RockSlide : MonoBehaviour {
 	}
 
 	private bool IsInSlowZone() {
-		return GetDistanceToPlayer() < slowZoneDistanceFromPlayer;
+		return GetDistanceToPlayer() < slowZoneWidth;
 	}
 
 	private void Update() {
-		if (InputManager.instance.GetMouseButtonDown(0) || InputManager.instance.GetTouchBegan()) hasStarted = true;
+		if (!hasStarted) {
+			if (InputManager.instance.GetMouseButtonDown(0) || InputManager.instance.GetTouchBegan()) {
+				hasStarted = true;
+				StartMoving();
+			}
+		}
 
 		if (updateType != WhitUpdateType.Update) return;
 		if (!hasStarted) return;
-		UpdateOffsetUpdate();
+		if (isMoving) MoveUpdate();
 	}
 
 	private void FixedUpdate() {
 		if (updateType != WhitUpdateType.FixedUpdate) return;
 		if (!hasStarted) return;
-		UpdateOffsetFixedUpdate();
-	}
-
-	private void PushBack(float amount) {
-		float pushBack = pushBackAmount * amount;
-		Vector2 currentOffset = follow.GetOffset();
-		currentOffset.x += pushBack;
-		SetOffset(currentOffset.x);
-		if (OnPushBack != null) OnPushBack.Invoke(amount);
+		if (isMoving) MoveFixedUpdate();
 	}
 	
-	private void UpdateOffsetUpdate() {
+	private void MoveUpdate() {
 		float deltaTime = Time.deltaTime * Time.timeScale;
-//		UpdateOffset(deltaTime);
 		Move(deltaTime);
 	}
 
-	private void UpdateOffsetFixedUpdate() {
+	private void MoveFixedUpdate() {
 		float deltaTime = Time.fixedDeltaTime * Time.timeScale;
-//		UpdateOffset(deltaTime);
 		Move(deltaTime);
 	}
 
 	private void Move(float deltaTime) {
 		float speed;
-		if (IsInSlowZone()) speed = 40;
-		else speed = 80;
-		if (IsInSlowZone()) Debug.Log("slow zone");
-		else Debug.Log("fast zone");
+		if (IsInSlowZone()) speed = slowZoneSpeed;
+		else speed = fastZoneSpeed;
 		Vector3 position = transform.position;
 		position.x += speed * deltaTime;
 		transform.position = position;
-	}
-
-	private void UpdateOffset(float deltaTime) {
-		currentOffsetChangeRate = offsetChangeRateRange.Lerp(invertedSpeedPercent);
-		Vector2 currentOffset = follow.GetOffset();
-		float extraOffset = currentOffsetChangeRate * deltaTime;
-		if (IsInSlowZone()) extraOffset *= slowZoneMultiplier;
-		float newOffsetX = currentOffset.x + extraOffset;
-
-		SetOffset(newOffsetX);
-	}
-
-	private void SetOffset(float offsetX) {
-		follow.SetOffsetX(Mathf.Max(0, offsetX));
 	}
 }
