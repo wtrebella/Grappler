29475b57fc4aff78c139d53e1100923f069d99e9
Whitaker Trebella <wtrebella@gmail.com>
2016-07-05 14:57:42 -0500

    bear is in; looks like shit

--- Grappler_Unity/Assets/_Scripts/Project/Player/Animation/PlayerAnimator.cs
+++ Grappler_Unity/Assets/_Scripts/Project/Player/Animation/PlayerAnimator.cs
@@ -5,28 +5,21 @@ using Spine;
 public class PlayerAnimator : MonoBehaviour {
 	private string clipName_grappling = "grappling";
 	private string clipName_falling = "falling";
-	private string clipName_onGround = "onGround";
+	private string clipName_onGround = "idle"; // TODO add on ground animation
 	private string clipName_idle = "idle";
-	private string clipName_skating = "idle";// TODO add skating animation
 
-	[SerializeField] private SkeletonAnimation topAnimation;
-	[SerializeField] private SkeletonAnimation bottomAnimation;
-	
+	[SerializeField] private SkeletonAnimation skeletonAnimation;
+
 	public void PlayGrapplingAnimations() {
 		PlayAnimations(clipName_grappling);
 	}
 
 	public void PlayFallingAnimations() {
 		PlayAnimations(clipName_falling);
-		bottomAnimation.state.SetAnimation(0, clipName_grappling, true);
 	}
 
 	public void PlayIdleAnimations() {
 		PlayAnimations(clipName_idle);
-	}
-
-	public void PlaySkatingAnimations() {
-		PlayAnimations(clipName_skating);
 	}
 
 	public void PlayOnGroundAnimations() {
@@ -34,7 +27,6 @@ public class PlayerAnimator : MonoBehaviour {
 	}
 
 	private void PlayAnimations(string clipName) {
-		if (topAnimation != null) topAnimation.state.SetAnimation(0, clipName, true);
-		if (bottomAnimation != null) bottomAnimation.state.SetAnimation(0, clipName, true);
+		if (skeletonAnimation != null) skeletonAnimation.state.SetAnimation(0, clipName, true);
 	}
 }
