7a8a15d14ba599dc0a021539725ef11da8239e5e
Whitaker Trebella <wtrebella@gmail.com>
2016-07-18 19:19:21 -0500

    trying to get border working with polydraw object but realizing i've gotta really revamp it

---
+++ Grappler_Unity/Assets/_Scripts/Project/Miscellaneous/PolydrawObjectBorder.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PolydrawObjectBorder : MonoBehaviour {
+	public Polydraw.PolydrawObject polydrawObject;
+
+	private void Awake() {
+
+	}
+	
+	private void Start() {
+	
+	}
+	
+	private void Update() {
+	
+	}
+}
---
+++ Grappler_Unity/Assets/_Scripts/Project/Miscellaneous/TerrainSpawnTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TerrainSpawnTest : MonoBehaviour {
+
+	private void Awake() {
+
+	}
+	
+	private void Start() {
+	
+	}
+	
+	private void Update() {
+	
+	}
+}
