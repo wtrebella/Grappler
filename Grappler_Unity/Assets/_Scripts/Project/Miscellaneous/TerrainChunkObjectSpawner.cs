f4091c045bd6003fe67de6606a50ef0208bdd4e7
Whitaker Trebella <wtrebella@gmail.com>
2016-07-20 15:07:07 -0500

    added materials to all the terrain chunks; starting work on incorporating new terrain into the game

---
+++ Grappler_Unity/Assets/_Scripts/Project/Miscellaneous/TerrainChunkObjectSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Polydraw;
+
+public class TerrainChunkObjectSpawner : MonoBehaviour {
+	public GameObject prefab;
+
+	private PolydrawObject poly;
+
+	private void Awake() {
+		poly = GetComponentInParent<PolydrawObject>();
+		if (poly == null) Debug.LogError("no PolydrawObject parent!");
+	}
+	
+	private void Start() {
+		if (poly == null) return;
+
+		SpawnObjectsAlongBorder();
+	}
+
+	private void SpawnObject(Vector2 point) {
+		GameObject spawnedObject = Instantiate(prefab);
+		spawnedObject.transform.SetParent(transform);
+		spawnedObject.transform.position = point;
+	}
+
+	private void SpawnObjectsAlongBorder() {
+		List<Vector2> borderPoints = poly.GetWorldBorderPoints();
+		foreach (Vector2 point in borderPoints) SpawnObject(point);
+	}
+	
+	private void Update() {
+	
+	}
+}
